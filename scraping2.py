4dde50899802d1a5749f711dbd4be197c7297bc7
Victor Cunha <113069099+victorcunha3@users.noreply.github.com>
2023-07-19 11:06:27 -0300

    Update theme

--- scraping2.py
+++ scraping2.py
@@ -1,20 +1,21 @@
 import csv
 import re
 import pandas as pd
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget, QPushButton, QFileDialog, QTableWidgetItem, QTableWidget
+import datetime
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget, QPushButton, QFileDialog, QTableWidgetItem, QTableWidget, QHeaderView
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from qt_material import apply_stylesheet, QtStyleTools, QUiLoader
 
-
-class MainWindow(QMainWindow):
+class MainWindow(QMainWindow, QtStyleTools):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Busca de Tanques")
-        self.setMinimumSize(400, 300)
+        self.setWindowTitle("Busca de Tanques(desenvolvido por Victor)")
+        self.setMinimumSize(500, 400)
 
         self.central_widget = QWidget(self)
         self.setCentralWidget(self.central_widget)
@@ -26,8 +27,12 @@ class MainWindow(QMainWindow):
         self.layout.addWidget(self.label)
 
         self.table = QTableWidget()
-        self.table.setColumnCount(3)
-        self.table.setHorizontalHeaderLabels(["Litros", "Capacidade", "Porcentagem"])
+        self.table.setColumnCount(4) 
+        self.table.setHorizontalHeaderLabels(["Data Execução", "Litros", "Capacidade", "Porcentagem"])
+        header = self.table.horizontalHeader()
+        header.setSectionResizeMode(0, QHeaderView.ResizeMode.Stretch)
+        header.setSectionResizeMode(1, QHeaderView.ResizeMode.ResizeToContents)
+        header.setSectionResizeMode(2, QHeaderView.ResizeMode.ResizeToContents)
         self.layout.addWidget(self.table)
 
         self.button = QPushButton("Buscar Tanques")
@@ -42,7 +47,7 @@ class MainWindow(QMainWindow):
         self.dados_tanques = []
 
     def buscar_tanques(self):
-        chromedriver = r"C:\Users\victo\Downloads\chromedriver_win32\chromedriver.exe"
+        chromedriver = r"C:\Users\Aprendiz\Downloads\chromedriver.exe"
         service = Service(chromedriver)
         driver = webdriver.Chrome(service=service)
         driver.get('https://xpert.com.br/atg/')
@@ -75,14 +80,19 @@ class MainWindow(QMainWindow):
             capacidade = capacidade_match.group() if capacidade_match else ''
 
             litros_text = litros_element.text
-            litros_match = re.search(r'[\d.]+', litros_text)
-            litros = litros_match.group() if litros_match else ''
+            litros_match = re.search(r'(\d+(\.\d+)?)', litros_text)
+            litros = litros_match.group(1) if litros_match else '0.0'
+
             porcentagem = porcentagem_element.text
 
+            data_execucao = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
             dados = {
+                "Data Execução": data_execucao,
                 "Litros": litros,
                 "Capacidade": capacidade,
                 "Porcentagem": porcentagem,
+                #"Total Litros": total_litros,
             }
 
             self.dados_tanques.append(dados)
@@ -92,27 +102,31 @@ class MainWindow(QMainWindow):
         self.table.setRowCount(len(self.dados_tanques))
 
         for row, dados in enumerate(self.dados_tanques):
+            data_execucao_item = QTableWidgetItem(dados["Data Execução"])
             litros_item = QTableWidgetItem(dados["Litros"])
             capacidade_item = QTableWidgetItem(dados["Capacidade"])
             porcentagem_item = QTableWidgetItem(dados["Porcentagem"])
 
-            self.table.setItem(row, 0, litros_item)
-            self.table.setItem(row, 1, capacidade_item)
-            self.table.setItem(row, 2, porcentagem_item)
+            self.table.setItem(row, 0, data_execucao_item)
+            self.table.setItem(row, 1, litros_item)
+            self.table.setItem(row, 2, capacidade_item)
+            self.table.setItem(row, 3, porcentagem_item)
 
         self.download_button.setEnabled(True)
 
     def download_data(self):
         file_dialog = QFileDialog()
-        file_path, _ = file_dialog.getSaveFileName(self, "Salvar arquivo", "", "Arquivo XLSX (*.xlsx)")
+        caminho_arquivo, _ = file_dialog.getSaveFileName(self, "Salvar arquivo", "", "Arquivo XLSX (*.xlsx)")
 
-        if file_path:
+        if caminho_arquivo:
             df = pd.DataFrame(self.dados_tanques)
-            df.to_excel(file_path, index=False)
+            df.to_excel(caminho_arquivo, index=False)
 
 
 if __name__ == "__main__":
     app = QApplication([])
     window = MainWindow()
+    apply_stylesheet(app, theme='dark_teal.xml', invert_secondary=True)
+
     window.show()
     app.exec()
